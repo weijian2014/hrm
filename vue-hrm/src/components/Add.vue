b01bc8b84b8b86f70b339de928f80434e50ef22d
weijian <303101610@qq.com>
2023-03-25 21:05:34 +0800

    update

--- vue-hrm/src/components/Add.vue
+++ vue-hrm/src/components/Add.vue
@@ -9,17 +9,17 @@
          :close-on-press-escape="isEscapeClose"
          :show-close="isShowClose"
          :close-on-click-modal="isClickModalToClose">
-         <el-form>
+         <el-form :model="rawFormData">
             <!-- 第1行 -->
             <el-row class="ml-6">
                <el-col :span="6">
                   <el-form-item label="姓名" prop="name">
-                     <el-input v-model="formData.name"> </el-input>
+                     <el-input v-model="rawFormData.name"> </el-input>
                   </el-form-item>
                </el-col>
                <el-col :span="6" class="mx-5">
                   <el-form-item label="性别" prop="gender">
-                     <el-select v-model="formData.gender" placeholder="">
+                     <el-select v-model="rawFormData.gender" placeholder="">
                         <el-option
                            v-for="item in genderOptions"
                            :key="item.value"
@@ -31,7 +31,7 @@
                <el-col :span="8">
                   <el-form-item label="生日" prop="birthday">
                      <el-date-picker
-                        v-model="formData.birthday"
+                        v-model="rawFormData.birthday"
                         type="date"
                         placeholder="" />
                   </el-form-item>
@@ -41,14 +41,14 @@
             <el-row class="ml-6">
                <el-col :span="6">
                   <el-form-item label="身高" prop="height">
-                     <el-input v-model="formData.height" placeholder="">
+                     <el-input v-model="rawFormData.height" placeholder="">
                         <template #append>cm</template>
                      </el-input></el-form-item
                   >
                </el-col>
                <el-col :span="6" class="mx-5">
                   <el-form-item label="体重" prop="weight">
-                     <el-input v-model="formData.weight" placeholder="">
+                     <el-input v-model="rawFormData.weight" placeholder="">
                         <template #append>kg</template>
                      </el-input></el-form-item
                   >
@@ -58,7 +58,8 @@
             <el-row class="ml-3">
                <el-col :span="20">
                   <el-form-item label="现住址" prop="current_address">
-                     <el-input v-model="formData.current_address"> </el-input>
+                     <el-input v-model="rawFormData.current_address">
+                     </el-input>
                   </el-form-item>
                </el-col>
             </el-row>
@@ -67,7 +68,7 @@
                <el-col :span="8">
                   <el-form-item label="政治面貌" prop="political_status">
                      <el-select
-                        v-model="formData.political_status"
+                        v-model="rawFormData.political_status"
                         placeholder="">
                         <el-option
                            v-for="item in politicalStatusOptions"
@@ -79,7 +80,7 @@
                </el-col>
                <el-col :span="6" class="mx-5">
                   <el-form-item label="学历" prop="degree">
-                     <el-select v-model="formData.degree" placeholder="">
+                     <el-select v-model="rawFormData.degree" placeholder="">
                         <el-option
                            v-for="item in degreeOptions"
                            :key="item.value"
@@ -91,7 +92,7 @@
                <el-col :span="6">
                   <el-form-item label="社保" prop="social_security">
                      <el-select
-                        v-model="formData.social_security"
+                        v-model="rawFormData.social_security"
                         placeholder="">
                         <el-option
                            v-for="item in socialSecurityOptions"
@@ -107,7 +108,7 @@
                <el-col :span="10">
                   <el-form-item label="身份证" prop="identifier">
                      <el-input
-                        v-model="formData.identifier"
+                        v-model="rawFormData.identifier"
                         :formatter="identifierFormatter"
                         placeholder="">
                      </el-input>
@@ -116,7 +117,7 @@
                <el-col :span="8" class="ml-5">
                   <el-form-item label="电话" prop="phone">
                      <el-input
-                        v-model="formData.phone"
+                        v-model="rawFormData.phone"
                         :formatter="phoneFormatter"
                         placeholder="">
                      </el-input>
@@ -127,13 +128,14 @@
             <el-row class="ml-3">
                <el-col :span="10">
                   <el-form-item label="原单位" prop="former_employer">
-                     <el-input v-model="formData.former_employer"> </el-input>
+                     <el-input v-model="rawFormData.former_employer">
+                     </el-input>
                   </el-form-item>
                </el-col>
                <el-col :span="10" class="ml-5">
                   <el-form-item label="参加工作时间" prop="first_work_time">
                      <el-date-picker
-                        v-model="formData.first_work_time"
+                        v-model="rawFormData.first_work_time"
                         type="date"
                         placeholder="" />
                   </el-form-item>
@@ -143,7 +145,7 @@
             <el-row class="ml-6">
                <el-col :span="6">
                   <el-form-item label="岗位" prop="post">
-                     <el-select v-model="formData.post" placeholder="">
+                     <el-select v-model="rawFormData.post" placeholder="">
                         <el-option
                            v-for="item in postOptions"
                            :key="item.value"
@@ -155,7 +157,7 @@
                <el-col :span="8" class="mx-5">
                   <el-form-item label="工资" prop="salary">
                      <el-input
-                        v-model="formData.salary"
+                        v-model="rawFormData.salary"
                         :formatter="salaryFormatter"
                         placeholder="">
                         <template #append>¥</template>
@@ -165,7 +167,7 @@
                <el-col :span="8">
                   <el-form-item label="保安证" prop="security_card">
                      <el-input
-                        v-model="formData.security_card"
+                        v-model="rawFormData.security_card"
                         :formatter="securityCardFormatter"
                         placeholder="">
                      </el-input>
@@ -176,7 +178,7 @@
                <el-col :span="20">
                   <el-form-item label="备注" prop="comments">
                      <el-input
-                        v-model="formData.comments"
+                        v-model="rawFormData.comments"
                         maxlength="800"
                         placeholder="填写了解的情况"
                         show-word-limit
@@ -196,7 +198,7 @@
 </template>
 
 <script setup lang="ts">
-import { watch, reactive, toRefs } from "vue"
+import { watch, reactive, toRefs, toRaw } from "vue"
 import Employee from "../class/Employee"
 
 // defineProps定义了当前组件的属性, 外部组件使用当前组件可以绑定传递进来
@@ -224,6 +226,7 @@ const state = reactive({
    isClickModalToClose: false, // 是否可以通过点击 modal 关闭 Dialog (对话框以外的任意位置)
    dialogVisible: false, // 是否显示对话框
    formLabelWidth: "10px",
+   rawFormData: new Employee(),
 })
 
 // 解构
@@ -233,37 +236,18 @@ const {
    isClickModalToClose,
    dialogVisible,
    formLabelWidth,
+   rawFormData,
 } = toRefs(state)
 
-// 当props中的formData变化到newValue时, 给form赋值, 而form被绑定给了el-form, 从而达到外部传送值给el-form的目的
-// watch(
-//    () => props.data,
-//    (newValue) => {
-//       if (newValue) {
-//          // 这时需要每个赋值, 不然formData将与props.data绑定在一起成为指向相同值的响应式对象
-//          formData.value = {
-//             id: newValue.id,
-//             name: newValue.name,
-//             gender: newValue.gender,
-//             birthday: newValue.birthday,
-//             first_work_time: newValue.first_work_time,
-//             salary: newValue.salary,
-//             post: newValue.post,
-//             social_security: newValue.social_security,
-//             phone: newValue.phone,
-//             former_employer: newValue.former_employer,
-//             height: newValue.height,
-//             weight: newValue.weight,
-//             degree: newValue.degree,
-//             political_status: newValue.political_status,
-//             identifier: newValue.identifier,
-//             security_card: newValue.security_card,
-//             current_address: newValue.current_address,
-//             comments: newValue.comments,
-//          }
-//       }
-//    }
-// )
+// watch写法上支持一个或者多个监听源, 这些监听源必须只能是getter/effect函数, ref数据, reactive对象或者数组类型
+watch(
+   () => props.formData,
+   (newValue) => {
+      // 当props.formData变化为newValue时, 拷贝一份原始数据,
+      // 不要直接使用props.formData, 否则会影响父组件的数据
+      rawFormData.value = { ...props.formData }
+   }
+)
 
 // 当props.isShow变化时会传递给dialogVisible, 而dialogVisible被绑定给了el-dialog, 从而达到外部控制显示隐藏el-dialog的目的
 watch(
