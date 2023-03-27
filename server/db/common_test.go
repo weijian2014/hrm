3f7fa3fcc73e3fe167eb7c2124abecb0251389b5
weijian <303101610@qq.com>
2023-03-27 22:31:28 +0800

    unit test for gorm

--- server/db/common_test.go
+++ server/db/common_test.go
@@ -3,6 +3,8 @@ package db
 import (
 	"os"
 	"testing"
+
+	"gorm.io/gorm"
 )
 
 func init() {
@@ -15,8 +17,8 @@ type Test struct {
 	Age  uint
 }
 
-func Test_Create_Insert(t *testing.T) {
-	test := &Test{Name: "hrm", Age: 18}
+func Test_Interface(t *testing.T) {
+	test1 := &Test{Name: "hrm1", Age: 18}
 
 	// 数据库文件存在时, 将删除
 	_, err := os.Stat(DatabaseFullPath)
@@ -30,97 +32,107 @@ func Test_Create_Insert(t *testing.T) {
 	}
 
 	// CreateTable
-	err = CreateTable(test)
+	err = CreateTable(test1)
 	if err != nil {
 		t.Error(err)
 		t.FailNow()
 	}
 	t.Logf("Create table ok")
 
-	err = Find(test, -1, "id = ?", test.Id)
-	if err != nil {
+	output1 := new(Test)
+	err = Take(output1, "id = ?", test1.Id)
+	if err != gorm.ErrRecordNotFound {
 		t.Error(err)
 		t.FailNow()
 	}
 
 	// Insert
-	err = Insert(test)
-	if err != nil {
+	err = Insert(test1)
+	if err != nil || test1.Id != 1 {
 		t.Error(err)
 		t.FailNow()
 	}
-	t.Logf("Insert ok, [%v]", test)
+	t.Logf("Insert ok, [%v]", test1)
 
 	// UpdateSingleColumn
-	err = UpdateSingleColumn(test, "age", 20)
+	err = UpdateSingleColumn(test1, "age", 20)
 	if err != nil {
 		t.Error(err)
 		t.FailNow()
 	}
-	if test.Age != 20 {
+	if test1.Age != 20 {
 		t.Errorf("No expected value")
 		t.FailNow()
 	}
 
-	test2 := &Test{Name: "hrm2"}
-	err = Take(test2, "id = ?", test.Id)
+	output2 := new(Test)
+	err = Take(output2, "id = ?", test1.Id)
 	if err != nil {
 		t.Error(err)
 		t.FailNow()
 	}
-	if test2.Age != 20 {
+	if output2.Age != 20 {
 		t.Errorf("No expected value")
 		t.FailNow()
 	}
-	t.Logf("Update single column ok, [%v]", test)
+	t.Logf("Update single column ok")
 
 	// UpdateRow
-	err = UpdateRow(test)
+	test1.Age = 88
+	err = UpdateRow(test1)
 	if err != nil {
 		t.Error(err)
 		t.FailNow()
 	}
 
-	test2.Age = 99
-	test2.Name = "unknown"
-	err = Take(test2, "id = ?", test.Id)
+	output3 := new(Test)
+	err = Take(output3, "id = ?", test1.Id)
 	if err != nil {
 		t.Error(err)
 		t.FailNow()
 	}
-	if test2.Age != 20 {
+	if output3.Age != 88 {
 		t.Errorf("No expected value")
 		t.FailNow()
 	}
-	t.Logf("Update row ok, [%v]", test)
+	t.Logf("Update row ok")
 
 	// SeleteAll
-	test3 := &Test{Name: "hrm3", Age: 22}
-	err = Insert(test3)
+	test2 := &Test{Name: "hrm3", Age: 33}
+	err = Insert(test2)
 	if err != nil {
 		t.Error(err)
 		t.FailNow()
 	}
+	t.Logf("Insert ok, [%v]", test2)
 	tests := new([]Test)
 	err = SelectAll(tests)
 	if err != nil || len(*tests) != 2 {
 		t.Error(err)
 		t.FailNow()
 	}
-	t.Logf("SeleteAll ok, [%v]", test)
+	t.Logf("SeleteAll ok")
 
 	// Delete
-	err = Delete(test, "id = ?", test.Id)
+	err = Delete(test1, "id = ?", test1.Id)
 	if err != nil {
 		t.Error(err)
 		t.FailNow()
 	}
-	err = Find(test2, -1, "id = ?", test.Id)
-	if err != nil {
+	output4 := new(Test)
+	err = First(output4, "id = ?", test1.Id)
+	if err != gorm.ErrRecordNotFound {
 		t.Error(err)
 		t.FailNow()
 	}
-	t.Logf("Delete row ok, [%v]", test)
+
+	output5 := new(Test)
+	err = Find(output5, 1, "id = ?", test2.Id)
+	if err != nil || output5.Age != test2.Age {
+		t.Error(err)
+		t.FailNow()
+	}
+	t.Logf("Delete row ok")
 
 	t.Logf("Test ok")
 }
